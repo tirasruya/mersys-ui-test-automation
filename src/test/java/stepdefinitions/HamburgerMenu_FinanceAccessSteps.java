86f00932e7b4c2ebd87d5c0aedd2d56b0109e1f6
tirasruya <tirasruya@gmail.com>
2026-01-22 22:50:31 +0300

    Features updated

--- src/test/java/stepdefinitions/HamburgerMenu_FinanceAccessSteps.java
+++
@@ -1,8 +0,0 @@
-package stepdefinitions;
-
-import org.openqa.selenium.WebDriver;
-import utils.BaseDriver;
-
-public class HamburgerMenu_FinanceAccessSteps {
-    WebDriver driver= BaseDriver.getDriver();
-}
